997e290027413e3e908cd31822b07394a21411a8
Cosmic-Coding-Community-Club <cosmiccodingcommunityclub@gmail.com>
2022-11-10 22:21:05 +0100

    remove section 1 solutions

--- src/main/kotlin/com/cccc/advancedkotlincourse/Section2CollectionPredicates.kt
+++ src/main/kotlin/com/cccc/advancedkotlincourse/Section2CollectionPredicates.kt
@@ -6,21 +6,21 @@ object Section2CollectionPredicates {
     
     // Return the boolean result in this function if the list of numbers meets the condition:
     // Exists some number with value "two"
-    fun task1() = numbers.any { it == "two" }
+    fun task1() = false
     
     // Return the boolean result in this function if the list of numbers meets the condition:
     // Not exists any number with value "eleven"
-    fun task2() = numbers.none { it == "eleven" }
+    fun task2() = false
     
     // Return the boolean result in this function if the list of numbers meets the condition:
     // All elements are at least 3 characters long
-    fun task3() = numbers.all { it.length >= 3 }
+    fun task3() = false
     
     // Return the boolean result in this function if the list of numbers meets the condition:
     // Exists some number with contains 'f' character
-    fun task4() = numbers.any { it.contains("f") }
+    fun task4() = false
     
     // Return the boolean result in this function if the list of numbers meets the condition:
     // Not exists any number that has more than 10 characters
-    fun task5() = numbers.none { it.length > 10 }
+    fun task5() = false
 }

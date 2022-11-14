e1d7b73db5be1582f843d05240fa08044cb193d0
Cosmic-Coding-Community-Club <cosmiccodingcommunityclub@gmail.com>
2022-11-14 22:14:24 +0100

    add section 3 constructors

---
+++ src/main/kotlin/com/cccc/advancedkotlincourse/creations/Car.kt
@@ -0,0 +1,7 @@
+package com.cccc.advancedkotlincourse.creations
+
+class Car(val color: String) {
+    
+    constructor(color: String, price: Float) : this(color)
+    
+}
---
+++ src/main/kotlin/com/cccc/advancedkotlincourse/creations/Tree.kt
@@ -0,0 +1,3 @@
+package com.cccc.advancedkotlincourse.creations
+
+class Tree(val type: String, var height: Int)
